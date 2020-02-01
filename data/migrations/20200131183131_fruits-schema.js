5d15f4c602c0f09107428cfa2a96ac581aea9000
Sean Hockin <sean.hockin@gmail.com>
2020-01-31 20:34:40 -0700

    Project complete

---
+++ data/migrations/20200131183131_fruits-schema.js
@@ -0,0 +1,13 @@
+
+exports.up = function(knex) {
+  return knex.schema.createTable('fruits', tbl => {
+      tbl.increments();
+      tbl.text('name', 128).unique().notNullable();
+      tbl.decimal('avgWeightOz').notNullable();
+      tbl.boolean('delicious');
+  })
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('fruits');
+};
--- knexfile.js
+++ knexfile.js
@@ -7,6 +7,12 @@ module.exports = {
     connection: {
       filename: './data/produce.db3'
     },
-    useNullAsDefault: true
+    useNullAsDefault: true,
+    migrations: {
+      directory: './data/migrations'
+    },
+    seeds: {
+      directory: './data/seeds'
+    }
   }
 };
